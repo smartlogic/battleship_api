5cdc8ae324e5154c9d282c2ddc14edbb11b65f0d
Jason Truluck <jason@smartlogic.io>
2018-03-12 15:21:47 -0400

    Initial commit of a simple battle ship ship list

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.json({routes: { ships: "/ships" } });
+})
+
+app.get('/ships', function (req, res) {
+  res.json({
+    aircraft_carrier: { name: 'Air Craft Carrier', length: 5 },
+    battleship: { name: 'Battleship', length: 4 },
+    submarine: { name: 'Submarine', length: 3 },
+    destroyer: { name: 'Destroyer', length: 3 },
+    patrol_boat: { name: 'Patrol Boat', length: 2 }
+  });
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
